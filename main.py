75157dc0bedc9e3f28e6413e164d44ee1be049eb
Артур <alutfullin43@gmail.com>
2026-03-27 20:39:22 +0300

    Изменен порядок: фильтры добавляются до регистрации хэндлеров

--- main.py
+++ main.py
@@ -19,12 +19,13 @@ crm = AlfaCRMClient(
 state_storage = StatePickleStorage(file_path="./states/states.pkl")
 bot = telebot.TeleBot(config.BOT_API_TOKEN, state_storage=state_storage)
 
-# Регистрация функций в боте
-register_base_handlers(bot, db, crm)
-register_parent_handlers(bot, db, crm)
 # Добавление фильтров для работы состояний
 bot.add_custom_filter(custom_filters.StateFilter(bot))
 bot.add_custom_filter(custom_filters.TextMatchFilter())
+
+# Регистрация функций в боте
+register_base_handlers(bot, db, crm)
+register_parent_handlers(bot, db, crm)
 
 if __name__ == "__main__":
     print("Бот запущен...")
