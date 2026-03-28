cecf64c16dbb5537214eb289d35df7447f1bf782
Артур <alutfullin43@gmail.com>
2026-03-28 17:37:34 +0300

    Добавлен URL на документ с политикой конфиденциальности

--- config.py
+++ config.py
@@ -14,3 +14,5 @@ TEACHER_PW_DIGEST = os.getenv("TEACHER_PASSWORD")
 
 BASE_DIR = os.path.dirname(os.path.abspath(__file__))
 DB_PATH = os.path.join(BASE_DIR, "db.json")
+
+PRIVACY_POLITIC_URL = os.getenv("PRIVACY_POLITIC_URL")
