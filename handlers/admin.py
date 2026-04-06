79f4d9d1dea89c8f931a9e135d21fb40b064d4ef
Артур <alutfullin43@gmail.com>
2026-04-06 21:40:12 +0300

    Добавлены фильтры в хэндлеры

--- handlers/admin.py
+++ handlers/admin.py
@@ -1,15 +1,15 @@
 from keyboards.admin_keyboards import admin_menu_kb, broadcast_roles_kb
 from utils import safe_send
 
-
 def register_admin_handlers(bot, db, crm):
     # ----- Админ-панель -----
-    @bot.message_handler(commands=["admin"])
+    @bot.message_handler(commands=["admin"], role="admin")
     def admin_start(message):
-        user_id = message.from_user.id
+        user_id = message.chat.id
         safe_send(bot, user_id, f"Добро пожаловать в панель управления", reply_markup=admin_menu_kb())
 
-    @bot.callback_query_handler(func=lambda call: call.data == "start_broadcast")
+    # ----- Рассылка -----
+    @bot.callback_query_handler(func=lambda call: call.data == "start_broadcast", role="admin")
     def start_broadcast(call):
         user_id = call.message.chat.id
         safe_send(bot, user_id, "Выберите для кого запустить рассылку:", reply_markup=broadcast_roles_kb())
