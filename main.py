a69799699044a40e1b02515f0cb9dbb88c408e26
Артур <alutfullin43@gmail.com>
2026-04-06 21:40:36 +0300

    Добавлены фильтры хэндлеров, изменен порядок регистрации хэндлеров

--- main.py
+++ main.py
@@ -10,6 +10,7 @@ from handlers.student import register_student_handlers
 from handlers.admin import register_admin_handlers
 from handlers.teacher import register_teacher_handlers
 from handlers.auth import register_all_auth_handlers
+from handlers.filters import RoleFilter
 
 # Инициализация базы и CRM
 db = Database()
@@ -26,13 +27,14 @@ bot = telebot.TeleBot(config.BOT_API_TOKEN, state_storage=state_storage)
 # Добавление фильтров для работы состояний
 bot.add_custom_filter(custom_filters.StateFilter(bot))
 bot.add_custom_filter(custom_filters.TextMatchFilter())
+bot.add_custom_filter(RoleFilter(db))
 
 # Регистрация функций в боте
-register_base_handlers(bot, db)
+register_admin_handlers(bot, db, crm)
 register_parent_handlers(bot, db, crm)
 register_student_handlers(bot, db, crm)
-register_admin_handlers(bot, db, crm)
 register_teacher_handlers(bot, db, crm)
+register_base_handlers(bot, db)
 register_all_auth_handlers(bot, db, crm)
 
 if __name__ == "__main__":
