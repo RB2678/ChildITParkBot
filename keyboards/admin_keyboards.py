cf994e0095a4eba7e2c1333fef98b40373fb392b
Артур <alutfullin43@gmail.com>
2026-04-15 21:24:02 +0300

    Добавлена клавиатура, ведущая в меню; клавиатура для навигации по списку должников

--- keyboards/admin_keyboards.py
+++ keyboards/admin_keyboards.py
@@ -20,7 +20,7 @@ def broadcast_roles_kb():
     }
 
     buttons = [
-        InlineKeyboardButton(text=name, callback_data=cb)
+        InlineKeyboardButton(text=name, callback_data="bc_role:" + cb)
         for name, cb in roles.items()
     ]
     buttons.append(InlineKeyboardButton(text="⬅️Назад", callback_data="back_to_menu"))
@@ -28,11 +28,39 @@ def broadcast_roles_kb():
 
     return keyboard
 
+
 def broadcast_confirming_kb():
     keyboard = InlineKeyboardMarkup(row_width=1)
     keyboard.add(
-        InlineKeyboardButton(text="подтвердить", callback_data="confirm_broadcast"),
+        InlineKeyboardButton(text="Подтвердить", callback_data="confirm_broadcast"),
         InlineKeyboardButton(text="⬅️Назад", callback_data="back_to_text")
     )
 
+    return keyboard
+
+
+def to_menu_kb():
+    kb = InlineKeyboardMarkup(row_width=1)
+    kb.add(InlineKeyboardButton(text="В меню", callback_data="to_menu"))
+    return kb
+
+
+def debtors_pagination_kb(current_page: int, total_pages: int):
+    keyboard = InlineKeyboardMarkup(row_width=2)
+
+    if total_pages <= 1:
+        keyboard.add(InlineKeyboardButton(text="В меню", callback_data="to_menu"))
+        return keyboard
+
+    if current_page == 0:
+        keyboard.add(InlineKeyboardButton(text="Вперед ➡️", callback_data=f"to_page_{current_page + 1}"))
+    elif current_page + 1 == total_pages:
+        keyboard.add(InlineKeyboardButton(text="⬅️ Назад", callback_data=f"to_page_{current_page - 1}"))
+    else:
+        keyboard.add(
+            InlineKeyboardButton(text="⬅️ Назад", callback_data=f"to_page_{current_page - 1}"),
+            InlineKeyboardButton(text="Вперед ➡️", callback_data=f"to_page_{current_page + 1}"),
+        )
+
+    keyboard.add(InlineKeyboardButton(text="В меню", callback_data="to_menu"))
     return keyboard
